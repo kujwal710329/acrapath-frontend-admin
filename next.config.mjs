015262e086a8ea5d1b7f329401dec6f3965aa1e8
Ujjwal Kumar <kujwal7103297@gmail.com>
2026-04-13 23:57:36 +0530

    added the suspense components in the code

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
       },
     ],
   },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
